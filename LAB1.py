9620ad86491b0b8e2c6024962d79971a59559e6f
Ahmedkhairy0106 <Ahmedkhairy0106@gmail.com>
2024-08-24 19:05:17 +0300

    Full Stack Web Development Using Python Lab 24/8

---
+++ LAB1.py
@@ -0,0 +1,38 @@
+# 24/5/2024 form 8 to 11 pm
+# ITI
+"""
+while True:
+    by = input("Enter your birthyear: ")
+    if by.isnumeric():
+        age = 2024 - int(by)
+        # f {age} ---> to add var in string
+        print(f"Welcome, You are {age} years old.")
+        break
+    else:
+        print("Invalid input. Please enter a valid birth year.")
+"""
+"""
+start_number = int(input("Enter Start number: "))
+end_number = int(input("Enter End number: "))
+for i in range(start_number,end_number + 1):
+    if(i % 2 == 0):
+        print(i)
+"""
+"""
+ysalary = int(input("Enter Your Salary: "))
+if(ysalary < 10000):
+    tax = ysalary * 0.1
+    result = ysalary - tax
+    print("Your tax is equal : ", tax)
+    print("Your salary after tax: ",result)
+elif(ysalary >= 10000 and ysalary <= 30000):
+    tax = ysalary * 0.15
+    result = ysalary - tax
+    print("Your tax is equal : ", tax)
+    print("Your salary after tax: ",result)
+else:
+    tax = ysalary * 0.20
+    result = ysalary - tax
+    print("Your tax is equal : ", tax)
+    print("Your salary after tax: ", result)
+"""
