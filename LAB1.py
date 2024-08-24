9c7fdfc4566be6c9c7a93427945854f029ed6c84
Ahmedkhairy0106 <Ahmedkhairy0106@gmail.com>
2024-08-24 20:54:33 +0300

    Full Stack Web Development Using Python Lab One 24/8

--- LAB1.py
+++ LAB1.py
@@ -1,5 +1,6 @@
 # 24/8/2024 form 8 to 11 pm
 # ITI
+
 """
 while True:
     by = input("Enter your birthyear: ")
@@ -11,28 +12,41 @@ while True:
     else:
         print("Invalid input. Please enter a valid birth year.")
 """
+
 """
-start_number = int(input("Enter Start number: "))
-end_number = int(input("Enter End number: "))
-for i in range(start_number,end_number + 1):
-    if(i % 2 == 0):
-        print(i)
+while True:
+    start_number = input("Enter Start number: ")
+    end_number = input("Enter End number: ")
+
+    if start_number.isnumeric() and int(start_number) >= 0:
+        if start_number.isdigit() and end_number.isdigit():
+            for i in range(int(start_number), int(end_number) + 1):
+                if i % 2 == 0:
+                   print(i)
+            break
+        else:
+           print("Invalid input. Please enter a valid input.")
+    else:
+        print("Please enter a positive start.")
+
 """
+
 """
-ysalary = int(input("Enter Your Salary: "))
-if(ysalary < 10000):
-    tax = ysalary * 0.1
-    result = ysalary - tax
-    print("Your tax is equal : ", tax)
-    print("Your salary after tax: ",result)
-elif(ysalary >= 10000 and ysalary <= 30000):
-    tax = ysalary * 0.15
-    result = ysalary - tax
-    print("Your tax is equal : ", tax)
-    print("Your salary after tax: ",result)
-else:
-    tax = ysalary * 0.20
-    result = ysalary - tax
-    print("Your tax is equal : ", tax)
-    print("Your salary after tax: ", result)
+while True:
+    ysalary = input("Enter Your Salary: ")
+    if ysalary.isnumeric():
+        ysalary = int(ysalary)
+        if ysalary < 10000:
+            tax = int(ysalary) * 0.1
+        elif ysalary >= 10000 and ysalary <= 30000:
+            tax = int(ysalary) * 0.15
+        else:
+            tax = int(ysalary) * 0.20
+
+        result = int(ysalary) - tax
+        print("Your tax is equal: ", tax)
+        print("Your salary after tax: ", result)
+        break
+    else:
+        print("Invalid input. Please enter a valid salary.")
 """
