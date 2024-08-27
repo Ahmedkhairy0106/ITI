0fc3941a6967313a7d8d68dfa4ff429c2f51371e
Ahmedkhairy0106 <Ahmedkhairy0106@gmail.com>
2024-08-27 03:37:15 +0300

    Full Stack Web Development Using Python Lab Two 25/8

---
+++ LAB2/Project 2/main2.py
@@ -0,0 +1,27 @@
+import json
+import random
+
+def generate_random_number():
+    random_numbers = [random.randint(1,100) for _ in range(10)]
+
+    with open("numbers.txt", "w") as f:
+        json.dump(random_numbers,f)
+        print("NUMBER IN FILES: ")
+        print(random_numbers)
+
+def sort_numbers_from_file():
+    numbers = []
+
+    with open("numbers.txt", "r") as f:
+        numbers = json.load(f)
+
+    sorted_number = sorted(numbers)
+
+    with open("sorted_numbers.txt", "w") as f:
+        json.dump(sorted_number,f)
+        print("\nNumber was added and sorted successfully")
+
+
+generate_random_number()
+
+sort_numbers_from_file()
